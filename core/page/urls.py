9b088250500c42a6e726eba517a3cf00a092395f
Gerardo A. Ballester Jr <gerardoaballesterjr@gmail.com>
2024-01-16 17:57:24 +0800

    update

--- core/page/urls.py
+++ core/page/urls.py
@@ -5,7 +5,4 @@ app_name = 'page'
 
 urlpatterns = [
     urls.path('', views.IndexView.as_view(), name='index'),
-    urls.path('about', views.AboutView.as_view(), name='about'),
-    urls.path('contact', views.ContactView.as_view(), name='contact'),
-    urls.path('privacy-policy', views.PrivacyPolicyView.as_view(), name='privacy-policy'),
 ]
